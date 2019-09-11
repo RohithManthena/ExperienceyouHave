3c7ad777bdb7dfd4a157143de560473f045d7594
RohithManthena <smanthena@mail.usf.edu>
2019-09-10 20:53:40 -0400

    Add project files.

---
+++ ExperienceyouHave/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExperienceyouHave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years experience do you have in professional programming?");
+            int Exp = int.Parse(Console.ReadLine());
+            for (int i = 0; i < Exp; i++)
+            {
+                Console.WriteLine("You Have" +  i.ToString()  + "years of Experience");
+
+            }
+        }
+    }
+}
