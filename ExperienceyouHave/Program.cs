f87fae6a6d813d1bfa790efd4fb3ee6422d32741
RohithManthena <smanthena@mail.usf.edu>
2019-09-10 21:07:00 -0400

    A

--- ExperienceyouHave/Program.cs
+++ ExperienceyouHave/Program.cs
@@ -8,7 +8,7 @@ namespace ExperienceyouHave
         {
             Console.WriteLine("How many years experience do you have in professional programming?");
             int Exp = int.Parse(Console.ReadLine());
-            for (int i = 0; i < Exp; i++)
+            for (int i = 0; i <= Exp; i++)
             {
                 Console.WriteLine("You Have "  + i.ToString() +  " years of Experience");
 
